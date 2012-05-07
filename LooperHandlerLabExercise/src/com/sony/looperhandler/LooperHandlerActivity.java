f47c4d68828f75d64555e4890166f4d182a3d24f
andersgoransson <anders.goransson@jayway.com>
2012-05-07 10:45:50 +0200

    Added LooperHandlerLabExercise project

---
+++ LooperHandlerLabExercise/src/com/sony/looperhandler/LooperHandlerActivity.java
@@ -0,0 +1,15 @@
+package com.sony.looperhandler;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LooperHandlerActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+	}
+
+}
