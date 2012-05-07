7469d1f7ae1bd9aebe17faffbab958890717c1db
andersgoransson <anders.goransson@jayway.com>
2012-05-07 09:49:32 +0200

    Added AsyncTaskLabExercise

---
+++ AsyncTaskLabExercise/src/com/sony/asynctasklab/AsyncTaskLabActivity.java
@@ -0,0 +1,16 @@
+package com.sony.asynctasklab;
+
+import com.sony.asynctasklab.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AsyncTaskLabActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+	}
+}
